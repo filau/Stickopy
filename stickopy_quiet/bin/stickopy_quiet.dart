36975a931b6234ec3257dd964af354db7b624b1a
Firmin L <45711398+Firmin-Launay@users.noreply.github.com>
2021-02-09 17:06:59 +0100

    Add volume name detection

--- stickopy_quiet/bin/stickopy_quiet.dart
+++ stickopy_quiet/bin/stickopy_quiet.dart
@@ -37,14 +37,42 @@ void main() {
         " ", ""); // Remove all useless infos in the output of the command.
 
     LineSplitter ls = new LineSplitter();
-    List<String> lines = ls.convert(output); // Divide the output into a List.
+    List<String> driveLetters =
+        ls.convert(output); // Divide the output into a List.
 
-    lines.removeWhere(
+    driveLetters.removeWhere(
         (content) => content == ""); // Remove empty elements of the list.
 
-    for (String deviceID in lines) {
+    for (String deviceID in driveLetters) {
       // Iterate through all disponible removable disks.
       print(deviceID);
+      Process.run('wmic', [
+        'logicaldisk',
+        'where',
+        'DeviceID="$deviceID"',
+        'get',
+        'VolumeName'
+      ]) // Check the volume name of the drives.
+
+          .then((result) {
+        String output = result.stdout;
+        output = output.replaceAll("VolumeName", "").replaceAll(
+            " ", ""); // Remove all useless infos in the output of the command.
+
+        LineSplitter ls = new LineSplitter();
+        List<String> volNames =
+            ls.convert(output); // Divide the output into a List.
+
+        volNames.removeWhere(
+            (content) => content == ""); // Remove empty elements of the list.
+
+        for (String volName in volNames) {
+          // Iterate through the names of the volume (there shouldn't be more than one, but who knows, it's Windows after all ¯\_(ツ)_/¯).
+          print(volName);
+        }
+      });
     }
   });
 }
+
+// Pour plus tard, commande de copie : "xcopy /E /I G:\ C:\Users\Firmin\Desktop\G_drive"
